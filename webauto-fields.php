29c45a5855d08043bba6cc9a09efaf9a267558c3
miso999 <michal.simoncic@gmail.com>
2016-11-03 19:51:13 +0100

    Initial commit

---
+++ webauto-fields.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Plugin Name: Webauto Fields
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Vlastné polia k testom (technické údaje, ceny...)
+Version: 1.0
+Author: miso
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
+
+
